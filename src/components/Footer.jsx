3d347ff8f7e0583c382c10b13452bdaed108d7bd
nehais <nehasinha82@gmail.com>
2025-02-03 12:31:33 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 import "../styles/Footer.css";
+import GitHubLogo from "../assets/gitOpen.png";
+
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <Link to="https://github.com/girsy01/HostAndToast_FE" target="_blank">
+        <img src={GitHubLogo} alt="GitHub" className="github-logo" />
+      </Link>
+    </div>
+  );
 };
 
 export default Footer;
