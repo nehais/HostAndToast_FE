1730c2d68eaf7cb8c3a67d71be04b3b6b7d2cfb1
Franziska Scheer <franzi-scheer@web.de>
2025-02-04 22:18:32 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import ErrorModal from "./components/ErrorModal";
-import AllMealsPage from "./pages/AllMealsPage";
-import AddMeal from "./pages/AddMeal";
-import ShoppingCart from "./pages/ShoppingCart";
 import SingleMeal from "./pages/SingleMeal";
 
 function App() {
