0d3393390d02dadbbd761f311b61cfa73d2b3c07
Franziska Scheer <franzi-scheer@web.de>
2025-02-18 19:35:59 +0100

    added not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "../styles/PageNotFound.css";
+import emptyPlateImage from "../assets/emptyPlate.png";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found">
+      <div className="container">
+        <h1>Oops! Looks like this plate is empty.</h1>
+        <img src={emptyPlateImage} />
+        <Link to="/">
+          <button>Back to the Menu</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
